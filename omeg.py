8a9ec034be64e92761fb83488a7a4489c7ebfc7d
Hau5ratz <16449790+Hau5ratz@users.noreply.github.com>
2018-02-27 17:06:42 -0500

    Update omeg.py

--- omeg.py
+++ omeg.py
@@ -85,7 +85,7 @@ class Hand(OmegleHandler):
         if '*their' in message or "they're" in message:
             self.out('*Notice* Warning you have been flagged for pedantry')
             self.out("Pedantry is a sign of intellectual insecurity") 
-            self.out("Your attempt to discredit someone through non-relevant attention to detail (probably classist based) only reveals your insecurities about your capacity to think critically and focus on the content" 
+            self.out("Your attempt to discredit someone through non-relevant attention to detail (probably classist based) only reveals your insecurities about your capacity to think critically and focus on the content") 
 
     def timer(self):
         if self.verbose:
