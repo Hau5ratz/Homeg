ca89b6a0fb129c2a6f2e58dfbab983f59990718f
Hau5ratz <16449790+Hau5ratz@users.noreply.github.com>
2018-06-12 13:00:51 -0400

    Update omeg.py

--- omeg.py
+++ omeg.py
@@ -59,7 +59,7 @@ class Hand(OmegleHandler):
     def _setup(self, omegle):
         """ Called by the Omegle class for initial additional settings """
         self.omegle = omegle
-        self.omegle.browser.set_proxies({"http": "1.165.160.168:3128"})
+        self.omegle.browser.set_proxies({"http": "101.200.89.170:8888"})
 
     def connected(self):
         """ Called when we are connected with a stranger """
